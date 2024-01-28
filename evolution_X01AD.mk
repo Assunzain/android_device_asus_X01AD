d7efd5302d84561a9e6575c19f27417aeb4c1d9b
AssunZain <Asepsunanda.zain@gmail.com>
2024-01-28 21:08:25 +0000

    X01AD: Bringup Evolution

--- evolution_X01AD.mk
+++ evolution_X01AD.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
-# Inherit some common DerpFest stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
@@ -33,8 +33,20 @@ TARGET_BOOT_ANIMATION_RES := 720
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
+# EvoX Stuff 
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_USES_PICO_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true 
+TARGET_SUPPORTS_GOOGLE_RECORDER := true 
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Device identifiers.
-PRODUCT_NAME := derp_X01AD
+PRODUCT_NAME := evolution_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BRAND := asus
