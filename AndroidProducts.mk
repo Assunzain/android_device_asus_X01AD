8335b501956e85f16c82dfbb54ad7962e1f2a4a8
AssunZain <Asepsunanda.zain@gmail.com>
2024-02-09 13:05:23 +0000

    X01AD: Miku bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/miku_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    miku_X01AD-user \
+    miku_X01AD-userdebug \
+    miku_X01AD-eng
 
