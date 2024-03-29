ea838422859ecedf9347a38b9e6ff4d5ebb05203
AssunZain <Asepsunanda.zain@gmail.com>
2024-03-29 17:02:41 +0000

    X01AD: Fix Offending entries: android.hardware.usb@1.0-service.basic android.hardware.wifi@1.0-service.legacy

--- device.mk
+++ device.mk
@@ -437,10 +437,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
@@ -453,9 +449,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.X01AD
 
 # Wifi
-PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy
-
 PRODUCT_PACKAGES += \
     libcld80211 \
     libwifi-hal-qcom \
