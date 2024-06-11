9a4ea2009015b34667c1d3a9538af819e7898c14
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-06-11 20:05:04 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -289,7 +289,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1.vendor
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.keymaster@3.0.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
