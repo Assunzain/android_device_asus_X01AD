9b6f1f437b1eed2c71fab34da5f13f9b9428b766
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-06-12 20:40:30 +0700

    X01AD: build new required RIL/Telephony packages.

--- device.mk
+++ device.mk
@@ -430,7 +430,17 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml
+    qti_telephony_utils.xml \
+    librmnetctl \
+    libxml2 \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils-prd \
+    qti_telephony_utils_prd.xml
     
 
 PRODUCT_PACKAGES += \
