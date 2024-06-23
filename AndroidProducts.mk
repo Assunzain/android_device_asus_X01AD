888c177697a1be9cd64bb3dca32c8a3e6307a398
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-06-23 22:08:06 +0700

    Bringup Nad~2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rr_X01AD.mk
+    $(LOCAL_DIR)/nad_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    rr_X01AD-user \
-    rr_X01AD-userdebug \
-    rr_X01AD-eng
+    nad_X01AD-user \
+    nad_X01AD-userdebug \
+    nad_X01AD-eng
