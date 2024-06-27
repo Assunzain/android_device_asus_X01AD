c517864e93bbd30f88f9864768a724d60d301c7e
AssunZain <Asepsunanda.zain@gmail.com>
2024-06-27 18:13:04 +0700

    add viper

--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -24,6 +24,8 @@ $(call inherit-product, device/asus/X01AD/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Audio MOD
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
