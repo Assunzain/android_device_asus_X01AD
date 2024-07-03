e871fad7170505f46795f04d64fd3764d42dfe27
AssunZain <Asepsunanda.zain@gmail.com>
2024-07-03 13:35:54 +0000

    Revert "Add matrix comp"
    This reverts commit d542ee00b54aa4ab771172f29ab81cd8cd580bbf.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,7 +121,6 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
