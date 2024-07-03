bfea4ff1ffa485ed563b2d2f9468521647e50fd1
AssunZain <Asepsunanda.zain@gmail.com>
2024-07-03 15:54:44 +0000

    X01AD: Move to new RFS install_symlink targets

--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -18,6 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
