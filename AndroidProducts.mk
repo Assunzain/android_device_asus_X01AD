9dd12fab15045c658f5f7b4519834b8b6da6768f
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-07-05 19:59:09 +0700

    havoc 3

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/havoc_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    havoc_X01AD-user \
+    havoc_X01AD-userdebug \
+    havoc_X01AD-eng
