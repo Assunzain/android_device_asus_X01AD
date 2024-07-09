237a64fa7f7e236b68aaca0d44d74abbd1ad7e0d
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-07-09 07:14:23 +0700

    Set to FP

--- havoc_X01AD.mk
+++ havoc_X01AD.mk
@@ -48,7 +48,8 @@ PRODUCT_MODEL := ASUS_X01AD
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
-BUILD_FINGERPRINT := "asus/WW_X01AD/ASUS_X01A_1:10/WW_Phone-202005071625/17.2018.2004.31-20200507:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="msm8953_64-user 10 WW_Phone-202005071625 17.2018.2004.31-20200507 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230705.001.A1 10217028 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys
