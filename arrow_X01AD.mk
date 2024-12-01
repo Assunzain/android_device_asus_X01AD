20f557bafad3c5febaefa0e3d389b82ca39223bb
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-12-01 19:35:25 +0700

    X01AD: Bringup arrow -1

--- arrow_X01AD.mk
+++ arrow_X01AD.mk
@@ -22,9 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Audio MOD
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
@@ -36,15 +33,13 @@ TARGET_BOOT_ANIMATION_RES := 720
 TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 FACE_UNLOCK_SUPPORTED := true
-RISING_CHIPSET := Snapdragon632
-RISING_BUILDTYPE := UNOFFICIAL
-RISING_MAINTAINER := Assunzain
-WITH_GMS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_CORE_GMS := true
+ARROW_MIPA := true
+ARROW_MAINTAINER := AssunZain (Zain)
+$(call inherit-product, vendor/arrow/config/common.mk)
+
 
 # Device identifiers.
-PRODUCT_NAME := lineage_X01AD
+PRODUCT_NAME := arrow_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BRAND := asus
