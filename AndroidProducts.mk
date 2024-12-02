832e611902a821f4e5c6bde307e1d5d330a6cf80
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-12-02 09:11:47 +0700

    Bringup colt -2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,9 +16,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01AD.mk
+    $(LOCAL_DIR)/colt_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01AD-user \
-    lineage_X01AD-userdebug \
-    lineage_X01AD-eng
+    colt_X01AD-user \
+    colt_X01AD-userdebug \
+    colt_X01AD-eng
