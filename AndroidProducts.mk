4b5ec6845518ae31d8fd08299d60d51101847c23
AssunZain <Asepsunanda.zain@gmail.com>
2025-02-28 10:43:46 +0000

    baikalify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,9 +16,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_X01AD.mk
+    $(LOCAL_DIR)/baikalos_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_X01AD-user \
-    derp_X01AD-userdebug \
-    derp_X01AD-eng
+    baikalos_X01AD-user \
+    baikalos_X01AD-userdebug \
+    baikalos_X01AD-eng
--- baikalos_X01AD.mk
+++ baikalos_X01AD.mk
@@ -26,13 +26,13 @@ $(call inherit-product, device/asus/X01AD/device.mk)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Inherit some common DerpFest OS stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/baikalos/config/common_full_phone.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Derp
-DERP_BUILDTYPE := Unofficial
+TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -42,7 +42,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := true
 
 # Device identifiers.
-PRODUCT_NAME := derp_X01AD
+PRODUCT_NAME := baikalos_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BRAND := asus
